d8afeac5c6f607c7ccd0694cca64cd7e6b1181d9
Rafał Nowakiewicz <rafal.nowakiewicz@gmail.com>
2021-02-26 20:25:15 +0100

    IMPLEMENT JUMPING VERTICAL TRACK TELEPORT INSTANT

--- src/player.ts
+++ src/player.ts
@@ -7,6 +7,11 @@ export class Player implements SpriteObject {
   private app: PIXI.Application;
   private player: PIXI.Sprite & { vx?: number; vy?: number };
 
+  private static readonly SPEED = 5;
+  private static readonly VERTICAL_TELEPORT = 70;
+  private static readonly NUM_OF_TRACKS = 3;
+  private static readonly START_TRACK_RELATIVE_POSITION_Y = 15;
+
   constructor(app) {
     this.app = app;
   }
@@ -14,7 +19,7 @@ export class Player implements SpriteObject {
   public async create(): Promise<void> {
     this.player = await loadSprite(this.app, 'assets/sprites/tram.png');
     this.player.x = 15;
-    this.player.y = this.app.renderer.screen.height - this.player.height - 15;
+    this.player.y = this.app.renderer.screen.height - this.player.height - Player.START_TRACK_RELATIVE_POSITION_Y;
 
     this.player.vx = 0;
     this.player.vy = 0;
@@ -37,58 +42,54 @@ export class Player implements SpriteObject {
     const up = new Keyboard('ArrowUp');
     const down = new Keyboard('ArrowDown');
 
-    left.press = () => {
-      this.player.vx = -5;
+    left.press = (): void => {
+      this.player.vx = -Player.SPEED;
     };
 
-    left.release = () => {
+    left.release = (): void => {
       if (!right.isDown) {
         this.player.vx = 0;
       }
     };
 
-    right.press = () => {
-      this.player.vx = 5;
+    right.press = (): void => {
+      this.player.vx = Player.SPEED;
     };
 
-    right.release = () => {
+    right.release = (): void => {
       if (!left.isDown) {
         this.player.vx = 0;
       }
     };
 
-    up.press = () => {
-      this.player.vy = -5;
-    };
-
-    up.release = () => {
-      if (!down.isDown) {
-        this.player.vy = 0;
+    up.press = (): void => {
+      if (
+        this.player.y - Player.VERTICAL_TELEPORT >=
+        this.app.renderer.screen.height -
+          this.player.height -
+          Player.START_TRACK_RELATIVE_POSITION_Y -
+          Player.VERTICAL_TELEPORT * (Player.NUM_OF_TRACKS - 1)
+      ) {
+        this.player.y -= Player.VERTICAL_TELEPORT;
       }
     };
 
-    down.press = () => {
-      this.player.vy = 5;
-    };
-
-    down.release = () => {
-      if (!up.isDown) {
-        this.player.vy = 0;
+    down.press = (): void => {
+      if (
+        this.player.y + Player.VERTICAL_TELEPORT <=
+        this.app.renderer.screen.height - this.player.height - Player.START_TRACK_RELATIVE_POSITION_Y
+      ) {
+        this.player.y += Player.VERTICAL_TELEPORT;
       }
     };
   }
 
   public onUpdate = (delta: number): void => {
-    // this.player.x = this.app.renderer.screen.width / 2;
-    // this.player.y = this.app.renderer.screen.height / 2;
-
-    // this.player.rotation += 0.01;
-
     this.player.x += this.player.vx;
     this.player.y += this.player.vy;
   };
 
   public onResize = (width: number, height: number): void => {
-    this.player.y = height - this.player.height - 15;
+    // to be implemented
   };
 }
